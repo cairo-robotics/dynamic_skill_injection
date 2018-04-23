71298024f376428b1ba98c3d7f4937d105982574
jeff <jeve0658@colorado.edu>
2018-04-23 15:48:06 -0600

    placed abs engine in module and node

--- dsi_world_state/nodes/world_state_node.py
+++ dsi_world_state/nodes/world_state_node.py
@@ -3,15 +3,24 @@
 import rospy
 import rosparam
 from world_state.read_gazebo import readGazebo
+from world_state.abstraction_engine import abstractionEngine
+
+
+def test_callback(event):
+     print 'Timer called at ' + str(event.current_real)
+     print world_space_dict
 
 
 if __name__ == '__main__':
     rospy.init_node('World_Reader')
     config_loc = rospy.get_param('/dsi/world_state/config_file_location')
     print config_loc
-    #TODO havent decided if I want to share world space dictionary with other things yet
+    # TODO havent decided if I want to share world space dictionary with other
+    # things yet
     world_space_dict = {}
     gazebo_reader = readGazebo(ws_dict=world_space_dict, json_path=config_loc )
+
+    abstraction_engine = abstractionEngine(world_space_dict)
 
     try:
         rospy.spin()
