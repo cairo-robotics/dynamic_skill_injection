c22a3b6096736c1a40cf97263709e8e8b9ee3881
Carl Mueller <cmuell89@gmail.com>
2018-05-01 14:55:14 -0600

    Failure resolution and notification services

--- dsi_failure_recognition/nodes/failure_notification_server.py
+++ dsi_failure_recognition/nodes/failure_notification_server.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 
 import rospy
-import json
-from functools import partial
 from failure_detection.task_failure import TaskFailureDetector
 from dynamic_skill_injection_msgs.srv import FailureNotification
 
-def detect_failure(req)
+
+def detect_failure(req):
     detector = TaskFailureDetector()
     return detector.handle_request(req)
+
 
 def failure_notification_server():
     rospy.init_node('failure_notification_server')
     s = rospy.Service('failure_notification_server', FailureNotification, detect_failure)
+    rospy.loginfo("failure_notification_server up an running!")
     rospy.spin()
 
-if __name__ == "__main__":\
+
+if __name__ == "__main__":
     try:
         failure_notification_server()
     except rospy.ROSInterruptException:
