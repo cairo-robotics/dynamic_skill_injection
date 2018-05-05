d8f566f34ef8a95a775bf3a934fddcc004aeeb46
Carl Mueller <cmuell89@gmail.com>
2018-05-04 23:45:51 -0600

    New node subscribing to human_command to get resolutions actions that are  published to resolution_actions to update FailureMapper

--- dsi_failure_recognition/nodes/resolution_library.py
+++ dsi_failure_recognition/nodes/resolution_library.py
@@ -2,35 +2,31 @@
 
 import rospy
 import json
+from failure_resolution import ResolutionLibrary
 from std_msgs.msg import String
 
-resolution_actions = {
-    "search": {
-        "turn_on_lights": {
-            "failure_conditions": {
-                "lights_on": False
-                }
-            },
-        "lift_object": {
-            "failure_conditions": {
-                "target_occluded": True
-            }
-        }
-    }
-}
 
+def generate_resolution_library():
 
-def publish_resolution_actions():
-    rospy.init_node('resolution_actions_publisher', anonymous=False)
+    rospy.init_node('resolution_library_publisher', anonymous=False)
+
+    library = ResolutionLibrary()
+
+    # We pass the bound detector method human_command_callback as the callback  for each the 
+    # /human_command subscrubers, which update the ResolutionLibrary instance's member variable.
+    rospy.Subscriber('/human_command', String,  library.human_command_callback)
+
     pub = rospy.Publisher('dsi/resolution_actions', String, queue_size=10)
-    rate = rospy.Rate(10)  # 10hz
+
+    rate = rospy.Rate(10)  # 10hz 
     while not rospy.is_shutdown():
-        pub.publish(json.dumps(resolution_actions))
+        message = json.loads(library.resolution_actions)
+        pub.publish(message)
         rate.sleep()
 
 
 if __name__ == '__main__':
     try:
-        publish_resolution_actions()
+        generate_resolution_library()
     except rospy.ROSInterruptException:
         pass
