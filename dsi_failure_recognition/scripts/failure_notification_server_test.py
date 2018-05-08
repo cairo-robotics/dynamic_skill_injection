abddaab2b8f4fdac0695ddfbbb8c7f4f3bc59e13
Carl Mueller <cmuell89@gmail.com>
2018-05-08 15:53:03 -0600

    Reverting back to old effect and worlstate example

--- dsi_failure_recognition/scripts/failure_notification_server_test.py
+++ dsi_failure_recognition/scripts/failure_notification_server_test.py
@@ -6,84 +6,33 @@ from dynamic_skill_injection_msgs.srv import FailureNotification
 
 # Not strictly required to have all data types, only those that are affected by the current task operator.
 effect = {
-    u'movo': 
-        {
-            u'location_str': [('in', 'red_5m_room')], 
-            u'orientation_rpq': [-6.599567256198967e-09, 5.255610232621944e-06, 2.5162805461869586e-05], 
-            u'location_xyz': [-0.4907107396040672, 5.060855618075925, -1.3289987767228784e-06], 
-            u'velocity': [-1.8916128783190173e-06, -0.00013714909174842698, -0.0010479390560907522, 8.011678144514673e-06, 0.006460918545800547, 2.568022993246033e-06]
-        }
+    "mug1": {
+        "type": "container",
+        "location_xyz": [0, 0, 0, 0, 0, 0],
+        "location_str": [["on", "table1"], ["in", "kitchen1"]],
+        "size": [5.0, 5.0, 5.0],
+        # Goal is to have detected go from False -> True
+        "detected": True,
+        "open": True,
+        "empty": True
     }
-
+}
 
 # Not strictly required to have all data types, only those that are affected by the current task operator.
 world_state = {
-    u'cafe_beer': 
-        {
-            u'location_str': 
-                [[u'in', u'folding_table_4x2'], [u'in', u'red_5m_room']], 
-                u'dimensions': [0.066, 0.066, 0.122], 
-                u'visible': True, 
-                u'velocity': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
-                u'orientation_rpq': [-0.0010953467905048702, 0.0005336966633416002, -2.905181720484242e-07], 
-                u'type': u'container', 
-                u'location_xyz': [0.8786726105705384, 5.383354107952973, 0.4285110216314646]
-        },
-    u'ground_plane': 
-        {
-            u'velocity': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
-            u'_comment': 
-            u'Bullshit', 
-            u'orientation_rpq': [0.0, -0.0, 0.0], 
-            u'type': 
-            u'none', 
-            u'location_xyz': [0.0, 0.0, 0.0]
-        }, 
-    u'movo': 
-        {
-            u'right_hand_empty': [u'True'], 
-            u'location_str': [[u'in', u'red_5m_room']], 
-            u'dimensions': [0.4, 0.2, 1.0], 
-            u'visible': u'True', 
-            u'carrying': [u'None'], 
-            u'left_hand_empty': [u'True'], 
-            u'velocity': [-1.6107279817900635e-06, -0.0001232466188071857, 0.0010479836455093226, -6.847722887102694e-06, -0.006460933546287295, 1.6820581433852563e-06], 
-            u'orientation_rpq': [-1.4790662860104751e-08, -1.2052738466292656e-06, 0.00020765243992752094], 
-            u'type': u'agent', 
-            u'location_xyz': [-0.49086115839172206, 5.049655676349373, -2.810418510135193e-07]
-        }, 
-    u'folding_table_4x2': 
-        {
-            u'location_str': [[u'in', u'red_5m_room']], 
-            u'dimensions': [1.0, 1.0, 0.5], 
-            u'_comment': u'dimensions are wrong need to scrape stl', 
-            u'location_xyz': [0.8196784853935242, 5.2346320152282715, 0.4285709857940674], 
-            u'velocity': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
-            u'orientation_rpq': [0.0, -0.0, 0.0], 
-            u'type': u'static', 
-            u'visibile': u'True'
-        }, 
-    u'blue_5m_room': 
-        {
-            u'location_str': [], 
-            u'door': u'unconfigured possibly location?', 
-            u'dimensions': [5.0, 5.0, 2.5], 
-            u'velocity': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
-            u'orientation_rpq': [0.0, -0.0, 0.0], 
-            u'type': u'room', 
-            u'location_xyz': [0.0, 2.220446049250313e-16, 0.0]
-        }, 
-    u'red_5m_room': 
-        {
-            u'location_str': [], 
-            u'door': u'unconfigured possibly location?',
-            u'dimensions': [5.0, 5.0, 2.5], 
-            u'velocity': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
-            u'orientation_rpq': [0.0, -0.0, 0.0], 
-            u'type': u'room', 
-            u'location_xyz': [0.009309999999999707, 5.0624, 0.0]
-        }
+    "mug1": {
+        "type": "container",
+        "location_xyz": [0, 0, 0, 0, 0, 0],
+        "location_str": [["in", "kitchen1"], ["on", "table1"]],
+        "size": [5.0, 5.0, 5.0],
+        # Goal is to have detected go from False -> True
+        "detected": False,
+        "lights_on": False,
+        "open": True,
+        "empty": True
     }
+}
+
 
 def failure_notification_client(effect, world_state):
     rospy.wait_for_service('failure_notification_server')
